6a0733b5c0f11a707e0c2ca8bc4af1703970f064
Tuniki Satwick <satwick.tuniki11@gmail.com>
2024-12-01 22:37:24 +0530

    first commit

---
+++ Models/UserContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Register.Models
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
