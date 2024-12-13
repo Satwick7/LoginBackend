e9518ba531594f206f7148a2ba4c1ac5d74fc280
Tuniki Satwick <satwick.tuniki11@gmail.com>
2024-12-13 16:47:49 +0530

    Bugs Has been fixed

--- obj/Release/net6.0/Register.AssemblyInfo.cs
+++ obj/Release/net6.0/Register.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Register")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a3f2a9ae6f310057fd08242007b6117974595cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Register")]
